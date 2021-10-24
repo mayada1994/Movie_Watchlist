83dc79d583236b19c209cf886bfdba367b00d5e7
Mayada Al-Sawah <mayada.alsawah@ekreative.com>
2021-10-24 04:56:30 +0300

    Add WatchlistFragment

---
+++ mvc/src/main/java/com/mayada1994/moviewatchlist/fragments/WatchlistFragment.kt
@@ -0,0 +1,22 @@
+package com.mayada1994.moviewatchlist.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mayada1994.moviewatchlist.databinding.FragmentWatchlistBinding
+
+class WatchlistFragment : Fragment() {
+
+    private lateinit var binding: FragmentWatchlistBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentWatchlistBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
